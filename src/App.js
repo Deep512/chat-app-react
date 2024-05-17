3cf19737a9e3252efd67d219645a1359a4baa80e
Deep <deep.dhanuka1@myntra.com>
2024-05-18 01:38:47 +0530

    minor cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import { useEffect, useState } from "react"
+import { useEffect } from "react"
 
 function App() {
-	const [counter, setCounter] = useState(0)
 	useEffect(() => {
 		fetch("http://localhost:8000/")
 			.then((response) => response.json())
 			.then((data) => console.log(data))
 	}, [])
 
-	return (
-		<div className="flex justify-center items-center h-screen">
-			<button
-				className="border border-10 p-4 bg-gray-400 rounded-md"
-				onClick={() => setCounter(counter + 1)}
-			>
-				{counter}
-			</button>
-		</div>
-	)
+	return <div className=""></div>
 }
 
 export default App
